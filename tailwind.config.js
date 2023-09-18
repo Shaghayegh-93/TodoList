0fa2cdaa4541e54207ccc99303f615665928acd0
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2023-09-18 20:22:17 +0100

    add astyle and class in tailwind layer

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Inter: ["Inter", "sans-serif"],
+        system: ["system-ui", "sans-serif"],
+        Avenir: ["Avenir", "sans-serif"],
+        Helvetica: ["Helvetica", "sans-serif"],
+        Arial: ["Arial", "sans-serif"],
+      },
+    },
+    colors: {
+      bgColor: "#f3f4f6",
+      text700: "#374151",
+      text400: "#94a3b8",
+      text300: "#cbd5e1",
+      white: "#fff",
+      primary700: "#4338ca",
+      primary600: "#4f46e5",
+      primary100: "#4f46e5",
+      rose500: "#f43f5e",
+    },
   },
   plugins: [],
 };
